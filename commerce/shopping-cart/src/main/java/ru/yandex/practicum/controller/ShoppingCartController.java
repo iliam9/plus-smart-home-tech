ff0af66efd2a981433353c6dfd3e63895660aa4a
iliam9 <ilkafa93@yandex.ru>
2025-03-23 01:32:18 +0300

    Add getUsersShoppingCart(), deactivateShoppingCart(), removeProductsFromShoppingCart(), changeProductQuantityInCart()

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/controller/ShoppingCartController.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/controller/ShoppingCartController.java
@@ -3,9 +3,11 @@ package ru.yandex.practicum.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.model.ChangeProductQuantityRequest;
 import ru.yandex.practicum.model.ShoppingCartDto;
 import ru.yandex.practicum.service.ShoppingCartService;
 
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -21,5 +23,31 @@ public class ShoppingCartController {
                                                     @RequestBody Map<UUID, Integer> products) {
         log.info("Received request by user: {} to add products to shopping cart", username);
         return shoppingCartService.addProductToShoppingCart(username, products);
+    }
+
+    @GetMapping
+    public ShoppingCartDto getUsersShoppingCart(@RequestParam String username) {
+        log.info("Received request to get user's {} shopping cart", username);
+        return shoppingCartService.getUsersShoppingCart(username);
+    }
+
+    @DeleteMapping
+    public void deactivateShoppingCart(@RequestParam String username) {
+        log.info("Received request to deactivate shopping cart of user: {}", username);
+        shoppingCartService.deactivateShoppingCart(username);
+    }
+
+    @PostMapping("/remove")
+    public ShoppingCartDto removeProductFromShoppingCart(@RequestParam String username,
+                                                         @RequestBody List<UUID> products) {
+        log.info("Received request to remove product from shopping cart of user: {}", username);
+        return shoppingCartService.removeProductsFromShoppingCart(username, products);
+    }
+
+    @PostMapping("/change-quantity")
+    public ShoppingCartDto changeProductQuantityInCart(@RequestParam String username,
+                                                       @RequestBody ChangeProductQuantityRequest request) {
+        log.info("Received request to change product quantity in shopping cart of user: {}", username);
+        return shoppingCartService.changeProductQuantityInCart(username, request);
     }
 }
--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/service/ShoppingCartServiceImpl.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/service/ShoppingCartServiceImpl.java
@@ -5,13 +5,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.yandex.practicum.exception.NotAuthorizedUserException;
 import ru.yandex.practicum.mapper.ShoppingCartMapper;
+import ru.yandex.practicum.model.ChangeProductQuantityRequest;
 import ru.yandex.practicum.model.ShoppingCart;
 import ru.yandex.practicum.model.ShoppingCartDto;
 import ru.yandex.practicum.model.ShoppingCartState;
 import ru.yandex.practicum.repository.ShoppingCartRepository;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -26,6 +29,7 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     @Override
     @Transactional
     public ShoppingCartDto addProductToShoppingCart(String username, Map<UUID, Integer> products) {
+        validateUsername(username);
         ShoppingCart shoppingCart = getShoppingCart(username);
         Map<UUID, Integer> oldProducts = shoppingCart.getProducts();
         oldProducts.putAll(products);
@@ -33,6 +37,56 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         shoppingCartRepository.save(shoppingCart);
         log.info("Products added to shopping cart: {}", shoppingCart);
         return shoppingCartMapper.mapToShoppingCartDto(shoppingCart);
+    }
+
+    @Override
+    public ShoppingCartDto getUsersShoppingCart(String username) {
+        validateUsername(username);
+        ShoppingCart shoppingCart = getShoppingCart(username);
+        log.info("Returning shopping cart of user {}", username);
+        return shoppingCartMapper.mapToShoppingCartDto(shoppingCart);
+    }
+
+    @Override
+    @Transactional
+    public void deactivateShoppingCart(String username) {
+        validateUsername(username);
+        ShoppingCart shoppingCart = getShoppingCart(username);
+        shoppingCart.setState(ShoppingCartState.DEACTIVATE);
+        shoppingCartRepository.save(shoppingCart);
+        log.info("Shopping cart of user {} is deactivated", username);
+    }
+
+    @Override
+    @Transactional
+    public ShoppingCartDto removeProductsFromShoppingCart(String username, List<UUID> products) {
+        validateUsername(username);
+        ShoppingCart shoppingCart = getShoppingCart(username);
+        Map<UUID, Integer> productMap = shoppingCart.getProducts();
+        productMap.keySet().retainAll(products);
+        shoppingCart.setProducts(productMap);
+        shoppingCartRepository.save(shoppingCart);
+        log.info("Product in shopping cart of user {} have been changed", username);
+        return shoppingCartMapper.mapToShoppingCartDto(shoppingCart);
+    }
+
+    @Override
+    @Transactional
+    public ShoppingCartDto changeProductQuantityInCart(String username, ChangeProductQuantityRequest request) {
+        validateUsername(username);
+        ShoppingCart shoppingCart = getShoppingCart(username);
+        Map<UUID, Integer> cartProducts = shoppingCart.getProducts();
+        cartProducts.put(request.getProductId(), request.getNewQuantity());
+        shoppingCart.setProducts(cartProducts);
+        shoppingCartRepository.save(shoppingCart);
+        log.info("Product quantity is changed to {}", request.getNewQuantity());
+        return shoppingCartMapper.mapToShoppingCartDto(shoppingCart);
+    }
+
+    private void validateUsername(String username) {
+        if (username.isBlank()) {
+            throw new NotAuthorizedUserException("Username is blank");
+        }
     }
 
     private ShoppingCart getShoppingCart(String username) {
