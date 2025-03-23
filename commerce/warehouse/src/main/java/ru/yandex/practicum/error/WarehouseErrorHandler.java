d9f8ecc86b906da4952eff6da584e586c503c86e
iliam9 <ilkafa93@yandex.ru>
2025-03-23 23:55:11 +0300

    Add WarehouseErrorHandler

---
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/error/WarehouseErrorHandler.java
@@ -0,0 +1,35 @@
+package ru.yandex.practicum.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.exception.*;
+
+@RestControllerAdvice
+public class WarehouseErrorHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleSpecifiedProductAlreadyInWarehouseException(SpecifiedProductAlreadyInWarehouseException e) {
+        return new ApiError(HttpStatus.BAD_REQUEST, e, "Product is already registered in warehouse");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleProductInShoppingCartLowQuantityInWarehouse(ProductInShoppingCartLowQuantityInWarehouse e) {
+        return new ApiError(HttpStatus.BAD_REQUEST, e, "Not enough products in warehouse");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleNoSpecifiedProductInWarehouseException(NoSpecifiedProductInWarehouseException e) {
+        return new ApiError(HttpStatus.BAD_REQUEST, e, "Selected product not in warehouse");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiError handleThrowable(Exception exception) {
+        return new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, exception, "Unexpected error");
+    }
+}
